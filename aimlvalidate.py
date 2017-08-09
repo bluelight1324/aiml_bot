f13e88242e0136d77a87be689e1bc3ed9958a5f1
Aaron Hosford <aaron.hosford@ericsson.com>
2017-08-08 22:11:56 -0500

    Cleaned up scripts

--- aimlvalidate.py
+++ aimlvalidate.py
@@ -11,7 +11,7 @@ Usage:
 # 1.0.1: Redirected stderr to stdout
 # 1.0: Initial release
 
-import aiml
+import aiml.aiml_parser
 import glob
 import sys
 import xml.sax
@@ -19,7 +19,7 @@ import xml.sax
 if __name__ == "__main__":
     # Need input file(s)!
     if len(sys.argv) < 2:
-        print __doc__
+        print(__doc__)
         sys.exit(2)
 
     # AimlParser prints its errors to stderr; we redirect stderr to stdout.
@@ -31,30 +31,30 @@ if __name__ == "__main__":
     for arg in sys.argv[1:]:
         # Input files can contain wildcards; iterate over matches
         for f in glob.glob(arg):
-            parser = xml.sax.make_parser(["aiml.AimlParser"])
-            handler = parser.getContentHandler()
+            parser = xml.sax.make_parser(["aiml.aiml_parser"])
+            handler = parser.getContentHandler()  # type: aiml.aiml_parser.AimlHandler
             docCount += 1
-            print "%s:" % f
+            print("%s:" % f)
             try:
                 # Attempt to parse the file.
                 parser.parse(f)
                 # Check the number of parse errors.
-                if handler.getNumErrors() == 0:    
+                if handler.getNumErrors() == 0:
                     validCount += 1
-                    print "PASSED\n"
+                    print("PASSED\n")
                 else:
-                    print "FAILED\n"
-            except xml.sax.SAXParseException, msg:
+                    print("FAILED\n")
+            except xml.sax.SAXParseException as msg:
                 # These errors occur if the document does not contain
                 # well-formed XML (e.g. open or unbalanced tags).  If
                 # they occur, parsing the whole document is aborted
                 # immediately.
-                print "FATAL ERROR: %s\n" % msg
+                print("FATAL ERROR: %s\n" % msg)
                 
     # Print final results
-    print "%d out of %d documents are AIML 1.0.1 compliant." % (validCount, docCount)
+    print("%d out of %d documents are AIML 1.0.1 compliant." % (validCount, docCount))
     if docCount == validCount:
-        print "Congratulations!"
+        print("Congratulations!")
     else:
-        print """For help resolving syntax errors, refer to the AIML 1.0.1 specification
-available on the web at: http://alicebot.org/TR/2001/WD-aiml"""
+        print("""For help resolving syntax errors, refer to the AIML 1.0.1 specification
+available on the web at: http://alicebot.org/TR/2001/WD-aiml""")
--- test.py
+++ test.py
@@ -16,27 +16,30 @@ kern = aiml.Kernel()
 brainLoaded = False
 forceReload = False
 while not brainLoaded:
-	if forceReload or (len(sys.argv) >= 2 and sys.argv[1] == "reload"):
-		# Use the Kernel's bootstrap() method to initialize the Kernel. The
-		# optional learnFiles argument is a file (or list of files) to load.
-		# The optional commands argument is a command (or list of commands)
-		# to run after the files are loaded.
-		kern.bootstrap(learnFiles="std-startup.xml", commands="load aiml b")
-		brainLoaded = True
-		# Now that we've loaded the brain, save it to speed things up for
-		# next time.
-		kern.saveBrain("standard.brn")
-	else:
-		# Attempt to load the brain file.  If it fails, fall back on the Reload
-		# method.
-		try:
-			# The optional branFile argument specifies a brain file to load.
-			kern.bootstrap(brainFile = "standard.brn")
-			brainLoaded = True
-		except:
-			forceReload = True
+    if forceReload or (len(sys.argv) >= 2 and sys.argv[1] == "reload"):
+        # Use the Kernel's bootstrap() method to initialize the Kernel. The
+        # optional learnFiles argument is a file (or list of files) to load.
+        # The optional commands argument is a command (or list of commands)
+        # to run after the files are loaded.
+        kern.bootstrap(learnFiles="std-startup.xml", commands="load aiml b")
+        brainLoaded = True
+        # Now that we've loaded the brain, save it to speed things up for
+        # next time.
+        kern.saveBrain("standard.brn")
+    else:
+        # Attempt to load the brain file.  If it fails, fall back on the Reload
+        # method.
+        try:
+            # The optional branFile argument specifies a brain file to load.
+            kern.bootstrap(brainFile="standard.brn")
+            brainLoaded = True
+        except Exception:
+            forceReload = True
 
 # Enter the main input/output loop.
-print "\nINTERACTIVE MODE (ctrl-c to exit)"
-while(True):
-	print kern.respond(raw_input("> "))
+print("\nINTERACTIVE MODE (ctrl-c to exit)")
+while True:
+    try:
+        print(kern.respond(input("> ")))
+    except KeyboardInterrupt:
+        break
